68bcb45bb0fef616c903084f867b6921e0f7b744
eunbi <eunbi@nethru.co.kr>
2019-04-22 18:32:49 +0900

    날짜 선택 원통 그리기 # TODO - scorll event

--- src/components/molecules/YearSelector.js
+++ src/components/molecules/YearSelector.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import '../../resources/sass/detail/YearSelector.scss';
 
+const YEAR_LENGTH = 10;
 class YearSelector extends Component {
+    state = {
+        angle: 36,
+    }
+    handleScroll= (e) => {
+        const lis = document.getElementById("picker").querySelectorAll("li");
+        lis.forEach(element => {
+            const re = new RegExp(/(\d.*)deg/);
+            let deg = parseInt(re.exec(element.style.webkitTransform)[1]);
+            deg += this.state.angle;
+            element.style.webkitTransform = 'rotateX(-'+deg+'deg) translateZ(120px)';
+        });
+    }
     render() {
+        console.log('render')
+        const currentYear = (new Date()).getFullYear();
+        const lis= [];
+        let deg = 0;
+        const angle = 360 / YEAR_LENGTH;
+        for (let i = 0 ; i < YEAR_LENGTH ; i ++) {
+            const style = {
+                webkitTransform: `rotateX(-${deg}deg) translateZ(120px)`
+            } 
+            lis.push(<li style={style}>
+                    {currentYear - i}
+                </li>);
+            deg += angle;
+        }
         return (
-            <div className="year-selector">
+            <div id="picker" className="year-selector" onScroll={this.handleScroll} onMo>
                 <ul>
-                    <li>2019</li>
-                    <li>2018</li>
-                    <li>2017</li>
-                    <li>2016</li>
-                    <li>2015</li>
-                    <li>2014</li>
-                    <li>2013</li>
-                    <li>2012</li>
+                    {lis}
                 </ul>
-            </div>
+             </div>
         );
     }
 }
