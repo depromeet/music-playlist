6e59b9dcefe7129e5ee466e1712db033c37b2f65
eunbi <silver23rain@gmail.com>
2019-04-23 03:34:02 +0900

    render시 translate y, z 넘기게 수정

--- src/components/molecules/YearSelector.js
+++ src/components/molecules/YearSelector.js
@@ -9,6 +9,10 @@ class YearSelector extends ScrollableSelector {
             list: [2019,2018,2017,2016,2015,2014,2013,2012,2011, 2010]
         })
     }
+
+    render() {
+        return super.render(-18, 140);
+    }
 }
 
 export default YearSelector;
